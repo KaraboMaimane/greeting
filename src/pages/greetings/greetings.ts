2aeecd56018fdf8140668df51092be0d08ed5033
Karabo Maimane <maimane23@hotmail.com>
2018-07-17 14:58:41 +0200

    This is the firs push of my app
    My first commit of the application

---
+++ src/pages/greetings/greetings.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the GreetingsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-greetings',
+  templateUrl: 'greetings.html',
+})
+export class GreetingsPage {
+  name;
+  surname;
+  gender: string;
+  styles = {};
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad GreetingsPage');
+  }
+
+  ionViewDidEnter() {
+    this.name = this.navParams.get('name');
+    this.surname = this.navParams.get('surname');
+  }
+
+  genderCheck(){
+    console.log(this.gender)
+  }
+}
